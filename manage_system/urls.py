0c8d10f51e88046c31324428fda4e5e05cf0e769
isabelccc <cxl0603@gmail.com>
2025-06-27 10:37:44 -0400

    updates database

--- manage_system/urls.py
+++ manage_system/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -37,7 +38,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def redirect_to_dashboard(request):
+    """Redirect root URL to dashboard"""
+    return redirect('/dashboard/')
+
 urlpatterns = [
+    path('', redirect_to_dashboard, name='home'),
     path('admin/', admin.site.urls),
     
     # Dashboard URLs
